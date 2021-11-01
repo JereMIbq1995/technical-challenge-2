8159c670ab3858ee481a503966e788ec50543e76
Jeremy Duong <jeremibq@gmail.com>
2021-11-01 14:54:05 -0600

    Completed work

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const app = express()
 const port = 3000
 app.use(express.static(__dirname));
 
+/*****************************************************************
+ * Home page
+ *****************************************************************/
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+/*****************************************************************
+ * Completely reset db.json to the initial state (db-initial.json)
+ *****************************************************************/
 app.post("/resetDb", (req, res) => {
     res.setHeader("Content-type", "application/text")
     fs.readFile(__dirname + '/db-initial.json', 'utf-8', (err, data) => {
@@ -28,6 +34,80 @@ app.post("/resetDb", (req, res) => {
     })
 })
 
+/*****************************************************************
+ * This endpoint is hit when the user want to progress an order
+ * to the next stage. For example, from Unassigned to In Progress.
+ * 
+ * This will update the status of the order in db.json
+ * 
+ * Return:
+ *      - 2 if there's anything wrong
+ *      - 1 if update succeeded
+ *****************************************************************/
+app.post("/updateOrderStatus", (req, res) => {
+    console.log(req.query);
+    // If the parameters don't add up to 3 and either
+    // orderId, currentStatus, or newStatus is missing,
+    // then it's not being called correctly
+    if (Object.keys(req.query).length != 3
+        || !req.query.orderId
+        || !req.query.currentStatus
+        || !req.query.newStatus) {
+        console.log("Query params error!");
+        res.send("2");
+        return;
+    }
+
+    res.setHeader("Content-type", "application/text")
+    
+    // Read from the database
+    fs.readFile(__dirname + '/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.send("2");
+            return;
+        }
+
+        // Look for the order in the db, up
+        let dataObject = JSON.parse(data);
+        let orderFound = false;
+        let updateValid = false;
+        for (order of dataObject.solarDesignOrders) {
+            if (order.id == req.query.orderId) {
+                orderFound = true;
+                if (order.status == req.query.currentStatus) {
+                    order.status = req.query.newStatus;
+                    updateValid = true;
+                }
+            }
+        }
+
+        // Fail if order is not found in the db, or the current
+        // status sent from front-end does not match.
+        // In the real world, this means that a different designer has taken
+        // the order and your page is not updated.
+        if (!orderFound || !updateValid) {
+            res.send("2");
+            return;
+        }
+
+        let updatedDataString = JSON.stringify(dataObject);
+
+        fs.writeFile(__dirname + '/db.json', updatedDataString, (err_w) => {
+            if (err_w) {
+                console.log(err_w);
+                res.send("2")
+                return;
+            }
+            res.status = 200;
+            res.send("1");
+        })
+    })
+})
+
+/*****************************************************************
+ * Return all the design orders stored in db.json
+ *****************************************************************/
 app.get("/api/designOrders", (req, res) => {
     fs.readFile(__dirname + '/db.json', 'utf-8', (err, data) => {
         let dataObject = JSON.parse(data);
@@ -42,6 +122,10 @@ app.get("/api/designOrders", (req, res) => {
     })
 })
 
+/******************************************************************
+ * I actually never used this end point but I just added it here for fun
+ * and in case I needed it.
+ ******************************************************************/
 app.get("/api/orderStatuses", (req, res) => {
     fs.readFile(__dirname + '/db.json', 'utf-8', (err, data) => {
         let dataObject = JSON.parse(data);
@@ -57,6 +141,12 @@ app.get("/api/orderStatuses", (req, res) => {
 })
 
 /******************************************************
+ * This function basically makes sure to return all the items
+ * in a list whose attributes match the query.
+ * 
+ * NOTE: Don't worrry too much about this function. It's just
+ *      an algorithm helper.
+ * 
  * Input:
  *  - query: all the parameters of a request
  *  - itemList: the list of objects in the db that we're
--- scripts/index.ts
+++ scripts/index.ts
@@ -3,26 +3,41 @@
 /**************************************************************
 * Author: Jeremy Duong
 ***************************************************************/
-let ORDERS = []
-let currentTab : Number = 1
 
+// Assign a constant for each of the 4 tabs
+const UNASSIGNED = 1;
+const IN_PROGRESS = 2;
+const COMPLETE = 3;
+const REQUESTED_REVISION = 4;
+
+let ORDERS = []
+let currentTab : Number = 0 // Initially, no tab is selected
+
+/**************************************************************
+* Given the tab id, determine which name to give its progress button
+***************************************************************/
 function getButtonName(tab : Number) {
-    if (tab == 1) {
+    if (tab == UNASSIGNED) {
         return "Claim";
-    } else if (tab == 2) {
+    } else if (tab == IN_PROGRESS) {
         return "Mark Complete";
-    } else if (tab == 3) {
+    } else if (tab == COMPLETE) {
         return "Request Revision";
     } else {
         return "Progress...";
     }
 }
 
-function updateTabLength() {
-    let lengthTab1 = 0;
-    let lengthTab2 = 0;
-    let lengthTab3 = 0;
-    let lengthTab4 = 0;
+/**************************************************************
+* Look at the status of each order (the stage it's in) to determine 
+* how many orders must be stored in each tab.
+* This function should be called every time any order's status changes
+***************************************************************/
+function updateTabsLength() {
+    let lengthTab1 = 0;   // UNASSIGNED
+    let lengthTab2 = 0;   // IN PROGRESS
+    let lengthTab3 = 0;   // COMPLETE
+    let lengthTab4 = 0;   // REQUESTED REVISION
 
     for (let order of ORDERS) {
         if (order.status == 1) {
@@ -36,107 +51,195 @@ function updateTabLength() {
         }
     }
 
+    // Update the number next to each tab
     document.getElementById("tab-length-1").innerHTML = String(lengthTab1);
     document.getElementById("tab-length-2").innerHTML = String(lengthTab2);
     document.getElementById("tab-length-3").innerHTML = String(lengthTab3);
     document.getElementById("tab-length-4").innerHTML = String(lengthTab4);
 }
 
-function progressOrderToNextStage(orderId : Number) {
+/**************************************************************
+* This function is the onclick handler of the progress button.
+***************************************************************/
+function progressOrder(event : any) {
+    // First, look at what is being clicked and its id
+    let orderId = Number((<HTMLAreaElement>event.currentTarget).id.slice(12));
+    let currentStatus = 0
     for (let order of ORDERS) {
         if (order.id == orderId) {
-            order.status += 1;
+            currentStatus = order.status;
+            break;
         }
     }
-    updateTabLength();
+    let newStatus = Number(currentStatus) + 1;
+
+    // A call to the server to update the order's status in the database
+    // This is probably not necessary
+    let queryString = "/updateOrderStatus?orderId=" + orderId + "&currentStatus=" + currentStatus + "&newStatus=" + newStatus;
+    fetch(queryString, {
+        method: 'POST'
+    })
+    .then((response) => response.text())
+    .then((responseText) => {
+
+        // 1 means it updated ok!
+        // If the update was a success, do the front-end stuff...
+        if (Number(responseText) == 1) {
+            // Remove that element from the page
+            document.getElementById("table-row-" + orderId).remove();
+
+            // Loop through all the orders to see which one needs to be progressed
+            // to the next stage
+            for (let order of ORDERS) {
+                if (order.id == orderId) {
+                    order.status += 1;
+                    break;
+                }
+            }
+
+            // Since an order's status has changed, the tabs' lengths need to be updated 
+            updateTabsLength();
+        } else {
+            alert("Update was unsucessful for some reason!");
+        }
+    })
 }
 
+/**************************************************************
+* Input: an order from the ORDERS list
+* Behavior:
+*   - Creates a <div> as a ".table-row" with the id of the order attached.
+*   - Creates a child <div> as a row entry to store the order's id
+*   - Creates a child <div> as a row entry to store the lead's name
+*   - Creates a child <div> as a row entry to store the date created
+*   - Creates a child <div> as a row entry to store the progress button
+    - Append the table row to the <div> with id "rows-container"
+***************************************************************/
 function appendTableRow(order : any) {
-    let targetDivElement = document.getElementById("table-rows");
+    // Where to append the row
+    let containerElement = document.getElementById("rows-container");
+
+    // Row element
     let rowElement = document.createElement("div");
-    rowElement.id = "table-row-" + order.id;
+    rowElement.id = "table-row-" + order.id; // So that we can remove it
     rowElement.className = "table-row";
 
+    // Create entries for orderId, leadName, date created, and progress button
     let idEntry = document.createElement("div");
     idEntry.className = "order-id";
     idEntry.innerHTML = order.id;
-
     let leadNameEntry = document.createElement("div");
     leadNameEntry.innerHTML = order.leadName;
-
     let dateCreatedEntry = document.createElement("div");
     dateCreatedEntry.innerHTML = order.dateCreated;
-
     let progressButton = document.createElement("div");
-    progressButton.className = "progress-button";
-    progressButton.id = "progress-id-" + order.id;
+    progressButton.className = "progress-button"; // For styling purposes
+    progressButton.id = "progress-id-" + order.id; // To know what we click on
     progressButton.innerHTML = getButtonName(order.status);
-    progressButton.onclick = function (event) {
+    progressButton.onclick = progressOrder;
 
-        let orderId = Number((<HTMLAreaElement>event.currentTarget).id.slice(12));
-        document.getElementById("table-row-" + order.id).remove();
-        progressOrderToNextStage(orderId);
-    }
-
+    //Append entries to row, row to table
     rowElement.appendChild(idEntry);
     rowElement.appendChild(leadNameEntry);
     rowElement.appendChild(dateCreatedEntry);
     rowElement.appendChild(progressButton);
-
-    targetDivElement.appendChild(rowElement);
+    containerElement.appendChild(rowElement);
 }
 
-function tabOnclickHandler(tab : Number) {
-    // first, mark the current tab as whatever is given
-    currentTab = tab;
-
-    // Next, mark the tab on the page as current by switching background to white
-    let targetTabElement = document.getElementById("tab-id-" + String(tab));
-    targetTabElement.style.background = "white";
-
-    // Look at all other tabs, if they're not currentTab, set background to blank
+/**************************************************************
+* Does 2 things:
+*    - Highlight the current tab with a white background
+*    - Assign empty background to other tabs
+***************************************************************/
+function handleTabsStyling() {
+    document.getElementById("tab-id-" + String(currentTab)).style.background = "white";
     for (let i = 1; i <= 4; i++) {
-        if (i != tab) {
+        if (i != currentTab) {
             document.getElementById("tab-id-" + String(i)).style.background = "";
         }
     }
+}
 
-    // Next, populate the table with appropriate data
-    document.getElementById("table-rows").innerHTML = ""
+/**************************************************************
+* This function is the onclick handler of the tabs
+***************************************************************/
+function tabOnclickHandler(event : any) {
+    // First, determine what was clicked on and its Id
+    let targetTabId = Number((<HTMLAreaElement>event.currentTarget).id.slice(7));
 
-    // Look at all the orders to see which one can be put in the currentTab
-    for (let order of ORDERS) {
-        if (order.status == tab) {
-            appendTableRow(order);
+    // Only do this if the clicked tab's id is different from the current id:
+    if (targetTabId != currentTab) {
+
+        // mark the current tab as whatever is given
+        currentTab = targetTabId;
+
+        // Style tab correctly so user can see visually which one is current
+        handleTabsStyling();
+
+        // Next, populate the table with appropriate data
+        // Clear the table first to avoid left-overs from other tabs
+        document.getElementById("rows-container").innerHTML = ""
+
+        // Look at all the orders to see which one can be put in the currentTab
+        for (let order of ORDERS) {
+            if (order.status == targetTabId) {
+                appendTableRow(order);
+            }
         }
     }
 }
 
+/**************************************************************
+* Add the onclick handler to all the tabs
+***************************************************************/
+function assignTabOnclickHandler() {
+    // Find all the "tab" elements
+    let tabElements = document.getElementsByClassName("tab");
+
+    // For each tab element, give it an onclick handler
+    for (let i = 0; i < tabElements.length; i++) {
+        (<HTMLAreaElement>tabElements[i]).onclick = tabOnclickHandler;
+    }
+}
+
+/**************************************************************
+* This function gets called immediately when the page is loaded.
+* Behavior:
+*   - Make HTTP Get request to the end point '/api/designOrders'
+*        to get all the desgin orders
+*   - Call updateTabsLength() and assignTabOnclickHandlers()
+***************************************************************/
 function getOrders() {
     fetch("/api/designOrders")
         .then((response) => response.json())
         .then((data) => {
+            // First, store all the orders for later use
             ORDERS = data;
-            updateTabLength();
-            return data;
         })
-        .then((data) => {
-            console.log(data);
-            tabOnclickHandler(currentTab);
+        .then(() => {
+
+            // Determine how many orders are in each tab and update accordingly
+            updateTabsLength();
+
+            // Add onclick handlers to the tabs
+            assignTabOnclickHandler();
         })
         .catch(error => {console.log(error)})
 }
 
-function appendTabOnclickEvents() {
-    let tabElements = document.getElementsByClassName("tab");
-
-    for (let i=0; i < tabElements.length; i++) {
-        (<HTMLAreaElement>tabElements[i]).onclick = function (event) {
-            let targetTabId = Number((<HTMLAreaElement>event.currentTarget).id.slice(7));
-            tabOnclickHandler(targetTabId);
-        }
-    }
-}
-
-appendTabOnclickEvents()
+// Call this function first when the page is loaded
 getOrders()
+
+/*****************************************************************
+ * This is for testing purposes. If you ever want to reset the database,
+ * just hit the Reset Database button and this will run.
+ *****************************************************************/
+function resetDatabase() {
+    fetch("/resetDb", {method: 'POST'})
+        .then(response => response.text())
+        .then((responseText) => {
+            console.log(responseText);
+            location.reload();
+        })
+}
+document.getElementById("resetDatabase").addEventListener("click", resetDatabase)
